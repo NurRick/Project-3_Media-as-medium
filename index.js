bb05e567acfa14f43472648f450c5a80e8834c56
NurRick <nmospanov@gmail.com>
2023-01-29 12:09:19 +0600

    added NYTimes

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 
-const articlesFromAPI = [
-    {
-        id: 0,
-        authorPhoto: "./assets/Arsen.jpg",
-        author: "Arsen Baiseupov",
-        topic: "Team news",
-        date: "Thursday 26 January 2023",
-        title: "EARLY TEAM NEWS FOR READING TIE",
-        summary:
-            "Manchester United are back in domestic cup action this weekend, when taking on Reading at Old Trafford.",
-        readingTime: "12 min read",
-        src: "./assets/Erik_.png",
-    },
-    {
-        id: 1,
-        authorPhoto: "./assets/Ereke.jpg",
-        author: "Yerkebulan Omar",
-        topic: "Team news",
-        date: "Thursday 26 January 2023",
-        title: "THE CELEBRATION THAT`S SWEEPING WORLD SPORT",
-        summary:
-            "The dazzling form Manchester United forward Marcus Rashford is currently experiencing under Erik ten Hag is no secret.",
-        readingTime: "12 min read",
-        src: "./assets/Marcus_.png",
+// const articlesFromAPI = [
+//     {
+//         id: 0,
+//         authorPhoto: "./assets/Arsen.jpg",
+//         author: "Arsen Baiseupov",
+//         topic: "Team news",
+//         date: "Thursday 26 January 2023",
+//         title: "EARLY TEAM NEWS FOR READING TIE",
+//         summary:
+//             "Manchester United are back in domestic cup action this weekend, when taking on Reading at Old Trafford.",
+//         readingTime: "12 min read",
+//         src: "./assets/Erik_.png",
+//     },
+//     {
+//         id: 1,
+//         authorPhoto: "./assets/Ereke.jpg",
+//         author: "Yerkebulan Omar",
+//         topic: "Team news",
+//         date: "Thursday 26 January 2023",
+//         title: "THE CELEBRATION THAT`S SWEEPING WORLD SPORT",
+//         summary:
+//             "The dazzling form Manchester United forward Marcus Rashford is currently experiencing under Erik ten Hag is no secret.",
+//         readingTime: "12 min read",
+//         src: "./assets/Marcus_.png",
 
-    },
-    {
-        id: 2,
-        authorPhoto: "./assets/Damir.jpg",
-        author: "Damir Shayakhmet",
-        topic: "United Now",
-        date: "Thursday 25 January 2023",
-        title: "DE GEA STOP UP FOR SAVE OF THE MONTH",
-        summary:
-            "David De Gea was at full stretch against Crystal Palace when keeping out a goalbound effort by Odsonne Edouard in our recent 1-1 draw.",
-        readingTime: "12 min read",
-        src: "./assets/David_.png",
-    },
-];
+//     },
+//     {
+//         id: 2,
+//         authorPhoto: "./assets/Damir.jpg",
+//         author: "Damir Shayakhmet",
+//         topic: "United Now",
+//         date: "Thursday 25 January 2023",
+//         title: "DE GEA STOP UP FOR SAVE OF THE MONTH",
+//         summary:
+//             "David De Gea was at full stretch against Crystal Palace when keeping out a goalbound effort by Odsonne Edouard in our recent 1-1 draw.",
+//         readingTime: "12 min read",
+//         src: "./assets/David_.png",
+//     },
+// ];
 
 let article = 
 `<section class="firstContent">
     <div class="firstContentText">
         <div class="fcIntroduse">
             <div class="fcIntAva" >
-                <img src="id=authorPhoto" alt="authorPhoto_img"/>
+                <img src="./assets/Ereke.jpg" alt="authorPhoto_img"/>
             </div>
             <div class="fcIntHead">
                 <h5 id="author"></h5>
@@ -67,7 +67,7 @@ let article =
                     </button>
                 </div>
                 <div>
-                    <p class="opacity" id="readingTime"></p>
+                    <p class="opacity" id="readingTime">12 min read</p>
                 </div>
                 <div>
                     <p> · </p>
@@ -96,21 +96,49 @@ let article =
 
 const articles =document.getElementById("articles");
 
-articlesFromAPI.forEach((item) => {
-    let newArticle = article.replace('src="id=authorPhoto"', `src="${item.authorPhoto}"`);
-    newArticle = newArticle.replace(
-        'id="author">',
-        `id="author">${item.author}`);
-    newArticle = newArticle.replace(
-        'id="topic">', 
-        `id="topic">${item.topic}`);
-    newArticle = newArticle.replace('id="date">', `id="date">${item.date}`);
-    newArticle = newArticle.replace('id="title">', `id="title">${item.title}`);
-    newArticle = newArticle.replace('id="summary">', `id="summary">${item.summary}`);
-    newArticle = newArticle.replace('id="readingTime">', `id="readingTime">${item.readingTime}`);
-    newArticle = newArticle.replace('src=""', `src="${item.src}"`);
+// articlesFromAPI.forEach((item) => {
+//     let newArticle = article.replace('src="id=authorPhoto"', `src="${item.authorPhoto}"`);
+//     newArticle = newArticle.replace(
+//         'id="author">',
+//         `id="author">${item.author}`);
+//     newArticle = newArticle.replace(
+//         'id="topic">', 
+//         `id="topic">${item.topic}`);
+//     newArticle = newArticle.replace('id="date">', `id="date">${item.date}`);
+//     newArticle = newArticle.replace('id="title">', `id="title">${item.title}`);
+//     newArticle = newArticle.replace('id="summary">', `id="summary">${item.summary}`);
+//     newArticle = newArticle.replace('id="readingTime">', `id="readingTime">${item.readingTime}`);
+//     newArticle = newArticle.replace('src=""', `src="${item.src}"`);
     
 
 
-    articles.innerHTML += newArticle;
-});
+//     articles.innerHTML += newArticle;
+// });
+
+
+
+async function fetchData() {
+    try {
+        const response = await fetch ('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=QkC0DyRG3Kn4NbfQHtgRIjYazOG55LXY');
+        const data = await response.json();
+        console.log(data.results);
+
+        data.results.forEach((item) => {
+            let newArticle = article.replace('id="author">',`id="author">${item.byline}`);
+            newArticle = newArticle.replace('id="topic">', `id="topic">${item.section}`);
+            newArticle = newArticle.replace('id="date">', `id="date">${item.published_date}`);
+            newArticle = newArticle.replace('id="title">', `id="title">${item.title}`);
+            newArticle = newArticle.replace('id="summary">', `id="summary">${item.abstract}`);
+            newArticle = newArticle.replace('src=""', `src="${item.multimedia[0].url}"`);
+            
+        
+        
+            articles.innerHTML += newArticle;
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchData();
